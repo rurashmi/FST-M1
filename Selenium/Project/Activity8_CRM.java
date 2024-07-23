55dbf8f633ab9e429eb977b056f50b7f26ddd502
rurashmi <rurashmi@in.ibm.com>
2024-07-23 15:39:12 +0530

    Add files via upload

---
+++ Selenium/Project/Activity8_CRM.java
@@ -0,0 +1,61 @@
+package project;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Activity8_CRM {
+    public static void main(String[] args) throws InterruptedException{
+        //Webdriver manager installation and set up
+        WebDriverManager.firefoxdriver().setup();
+        //Webdriver instantiation
+        WebDriver driver = new FirefoxDriver();
+        //Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        //Open URL
+        driver.get("http://alchemy.hguy.co/crm");
+        //FInd the web elements
+        WebElement username = driver.findElement(By.id("user_name"));
+        WebElement password = driver.findElement(By.id("username_password"));
+        //Provide credentials
+        username.sendKeys("admin");
+        password.sendKeys("pa$$w0rd");
+        //CLick on login button
+        driver.findElement(By.id("bigbutton")).click();
+        //Wait for home page
+        WebElement dashboard = driver.findElement(By.id("tab0"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("tab0")));
+        System.out.println("Home page title after login is: "+ driver.getTitle());
+        System.out.println(dashboard.getText());
+        Assertions.assertEquals("SUITECRM DASHBOARD", dashboard.getText());
+        //Actions object
+        Actions builder = new Actions(driver);
+
+        // //Navigate to  Sales -> Accounts page
+        // Clicking on the Hidden SubMenu
+        WebElement Sales = driver.findElement(By.id("grouptab_0"));
+        builder.moveToElement(Sales).click().build().perform();
+        driver.findElement(By.id("moduleTab_9_Accounts")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='moduleTitle']/h2")));
+        //Name and user columns
+        List<WebElement> name = driver.findElements(By.xpath("//table/tbody/tr/td[3]/b/a"));
+
+        //row iteration
+        for(int i=0; i<=10; i++) {
+            //check column each in row, identification with 'td' tag
+            System.out.println(name.get(i).getText());
+                    }
+        //Close the browser
+        driver.quit();
+
+    }
+}
